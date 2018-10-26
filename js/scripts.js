ccc6c113d9a7fdb5f67c5b18b84c77202ab3442a
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2018-10-26 10:28:47 -0700

    complete spec 3, change numbers containing 1 to boop

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,11 @@ var beepboop = function(num) {
 var arr = [];
 for (number=0 ; number <= num ; number++) {
   var numStr = number.toString();
-  if (numStr.includes("1")) {
-    numStr= "Boop!";
+  if (parseInt(numStr)%3===0 && numStr !=="0") {
+    numStr = "I'm sorry, Dave. I'm afraid I can't do that.";
+  }
+  else if (numStr.includes("1")) {
+    numStr = "Boop!";
   } else if (numStr.includes("0")) {
     numStr = "Beep!";
   }
