b3a7286899a2791b6d6e965ff830a79dff56fb6d
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2018-10-26 10:47:12 -0700

    complete spec 4, replace numbers divisible by 3 with phrase

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 // Business Logic
-var beepboop = function(num) {
 var arr = [];
-for (number=0 ; number <= num ; number++) {
-  var numStr = number.toString();
-  if (parseInt(numStr)%3===0 && numStr !=="0") {
-    numStr = "I'm sorry, Dave. I'm afraid I can't do that.";
-  }
-  else if (numStr.includes("1")) {
-    numStr = "Boop!";
-  } else if (numStr.includes("0")) {
-    numStr = "Beep!";
-  }
-  console.log(numStr);
+var beepboop = function(num) {
+  for (number=0 ; number <= num ; number++) {
+    var numStr = number.toString();
+    if (parseInt(numStr)%3===0 && numStr !=="0") {
+      numStr = "I'm sorry, Dave. I'm afraid I can't do that.";
+    }
+    else if (numStr.includes("1")) {
+      numStr = "Boop!";
+    } else if (numStr.includes("0")) {
+      numStr = "Beep!";
+    }
+    console.log(numStr);
+    arr.push(numStr);
+  };
+  console.log(arr);
 
-};
 };
 
 // User Interface Logic
@@ -24,7 +26,7 @@ $(document).ready(function() {
     var input = parseInt($("#numberInput").val());
     var result = beepboop(input);
     $("#resultOutput").show();
-    $("#resultList").append("<li>" + number + "</li>");
+    $("#resultList").text(arr.toString());
 
   });
 
